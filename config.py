6cf270194a1f9e4161f802bfeef14d179dd1107b
Buddy <nvsmxsoqpmifsggtxz@enotj.com>
2025-10-25 03:17:28 +0530

    ᴀʟʟʙᴏᴛsᴜᴘᴘᴏʀᴛ

--- config.py
+++ config.py
@@ -1,7 +1,4 @@
-BQFMsJ0ASjjorZ8hjDZs3Dcm2Zo_D91wYeosTuTxPDtn4ZxJwtOba30i2O8n8kM4xbIitU4J5P6sn00_hdgWp2hBSroLFolv9m0PY-x3gH_PUEjnBQGeu9nqVtv_MwXWu9jWjdhnCjGnRYpMyj8W7XjoagUDgJuBYKxutiT2lwK0tX-mcKi49P6cZeYS_HVrliRb2DKbJs_GYInFjQcJfEaKEscuc3mIvC4UggeLu-ms1Gk-BBTf7pm_C1sLGrjZGOXTguphjHQ1edAwAOKMcAh02brBnTt0RFf94662YSJ6bdKLbUvRmwguDxAhC-MdaDXs1kqxmiRBPjPem5Ys1QMw4c7_cwAAAAHpMPeqAA")
-
-# MongoDB (for saving user data)
-MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://IVANxNISHA:IVANxNISHA@cluster0.pq52raw.mongodb.net/?retryWrites=true&w=maj# config.py
+# config.py
 
 API_ID = 22657083      # Your Telegram API ID
 API_HASH = "d6186691704bd901bdab275ceaab88f3"
